74600d46d1bf462544e3801b5782a84c8032b0f0
scottskinn <84105761+scottskinn@users.noreply.github.com>
2021-06-18 13:11:30 -0400

    can answer questions but with errors

--- assets/script.js
+++ assets/script.js
@@ -18,23 +18,95 @@ let option2 = document.querySelector("#option2")
 let option3 = document.querySelector("#option3")
 let option4 = document.querySelector("#option4")
 
+let choice_que = document.querySelectorAll(".choice_que");
+
+// once clicked on button it should take you to the next question
+let next_question = document.querySelector("#next_question");
+let button = document.querySelectorAll("button");
+
+var i = 0;
+
 let index = 0;
-let timer = 0;
+let timer = 25;
 let interval = 0;
+
+let correct = 0;
+let answer = 0;
+
+let UserAns = undefined;
 
 start.addEventListener('click', ()=> {
     start.style.display = "none";
     rules.style.display = "none";
     quiz.style.display = "block";
+
+    // timer start
+    time = 0;
+    interval = setInterval(countDown, 1000);
+    loadData();
 })
 
 let countDown = ()=>{
     if (timer === 0){
         clearInterval(interval);
+        next_question.click();
     }
     else {
     timer--;
+    time.innerText = timer;
     console.log(timer);
     }
 }
+
+// setinterval(countDown,1000);
+
+let loadData = ()=>{
+    questionNo.innerText = index + 1 + ". ";
+    questionText.innerText = MYQS[index].question;
+    option1.innerText = MYQS[index].choice1;
+    option2.innerText = MYQS[index].choice2;
+    option3.innerText = MYQS[index].choice3;
+    option4.innerText = MYQS[index].choice4;
+}
+
+loadData();
+
+choice_que.forEach( (choices, choiceNo) =>{
+    choices.addEventListener("click", ()=>{
+        choices.classList.add("active");
+
+        if(choiceNo === MYQS[index].answer)
+        {
+            correct++;
+        } else {
+            correct += 0;
+        }
+        // disable all other options
+        for(i = 0; i <= 3; i++) {
+            choice_que[i].classList.add("disbled")
+        }
+
+    })
+});
+
+next_question.addEventListener("click", ()=> {
+    // go to next question
+    if(index !== MYQS.lenght - 1) {
+        index++;
+    }
+    var answerText = event.target.textContent
+    if(answerText === question[i].answer) {
+        timer = timer;
+    } else {
+        timer = timer - 15;
+    }
+
+    event.addEventListener("click", next_question)
+
+
+    // choice_que.addEventListener("click", ()=>{
+    //     removeActive.classList.remove("active");
+// })
+    loadData();
+})
 
